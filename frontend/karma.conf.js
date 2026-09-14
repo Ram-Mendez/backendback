045f892efa11189e987f939e53209506ece4e758
RamMendez(RaulMendez) <Balaramji95@gmail.com>
2026-09-14 08:48:50 +0200

    ci: configure Chrome no-sandbox correctly

---
+++ frontend/karma.conf.js
@@ -0,0 +1,15 @@
+module.exports = function (config) {
+    config.set({
+        browsers: ['ChromeHeadlessNoSandbox'],
+
+        customLaunchers: {
+            ChromeHeadlessNoSandbox: {
+                base: 'ChromeHeadless',
+                flags: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox'
+                ]
+            }
+        }
+    });
+};
